f9f97b5b0664b1bc66f14110ec90a86a146e5306
Asad Sheikh <asadalis@andrew.cmu.edu>
2020-10-22 04:30:33 -0500

    finished descriptions and added ml model

---
+++ ml_model.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score,f1_score,precision_score, recall_score
+
+df = pd.read_csv('https://data.cityofchicago.org/resource/ijzp-q8t2.csv')
+
+df_filtered = df.drop(columns = ['id', 'case_number', 'date', 'description', 'location_description', 'x_coordinate', 'y_coordinate', 'updated_on', 'latitude', 'longitude', 'location', 'fbi_code', 'block', 'district', 'ward', 'community_area', 'block', 'iucr'])
+df_filtered = df_filtered.dropna(axis=0)
+df_filtered = df_filtered[df_filtered['year']==2020]
+df = df_filtered
+X = df_filtered.drop(columns=['arrest'])
+df['beat'] = df['beat'].astype('category')
+# df['district'] = df['district'].astype('category')
+# df['ward'] = df['ward'].astype('category')
+# df['community_area'] = df['community_area'].astype('category')
+# df['block'] = df['block'].astype('category')
+# df['iucr'] = df['iucr'].astype('category')
+df['primary_type'] = df['primary_type'].astype('category')
+
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+# We create the preprocessing pipelines for both numeric and categorical data.
+numeric_features = ['year']
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())])
+
+# categorical_features = ['iucr', 'primary_type', 'domestic', 'beat', 'district', 'ward', 'community_area']
+categorical_features = ['primary_type', 'domestic', 'beat']
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)])
+
+# Append classifier to preprocessing pipeline.
+# Now we have a full prediction pipeline.
+clf = Pipeline(steps=[('preprocessor', preprocessor),
+                      ('classifier', DecisionTreeClassifier())])
+
+X = df.drop('arrest', axis=1)
+y = df['arrest']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf.fit(X_train, y_train)
+# print("model score: %.3f" % clf.score(X_test, y_test))
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+from ml_model import *
 
 #### Title ####
 
@@ -68,6 +69,10 @@ st.write(chart3 | background + hists)
 #     selection
 # )
 
+#### Number of Crimes vs. Arrest Rates ####
+st.markdown("<h2>Arrest Rates Over Time</h2>", unsafe_allow_html=True)
+st.write("Now that we've explored changes in types of crime over time, what is the trend for frequency of crimes and arrest rates?")
+st.write("Hover over each line to see change in frequency and arrest rate for that particular type of crime.")
 
 #crime_url = "https://data.cityofchicago.org/resource/ijzp-q8t2.json?$select=district,count(district),sum(case(arrest='1',1,true,0))&$group=district"
 #return pd.read_json(crime_url)
@@ -111,6 +116,11 @@ arrests_lines = arrests_base.mark_line(interpolate='basis').encode(
 )
 
 st.write(crimes_points+crimes_lines | arrests_points+arrests_lines)
+
+#### Mapping Crimes ####
+st.markdown("<h2>Location of Arrests</h2>", unsafe_allow_html=True)
+st.write("What is the distribution of crimes over the city?")
+st.write("Pan over the map to select an area to view and adjust the range of year.")
 
 #chart.encode(y='num_crimes:Q') | chart.encode(y="arrest_rate:Q")
 #st.write(chart)
@@ -161,6 +171,8 @@ chart2 = alt.layer(
 #st.write(chart2)
 
 
+
+
 @st.cache
 def load_coordinate_data(url):
     df = pd.read_json(url)
@@ -208,6 +220,10 @@ def load_district_data(url):
 
 #st.write(district_data)
 
+#### District Breakdown ####
+st.markdown("<h2>Frequency of Crime and Arrest Rate per District</h2>", unsafe_allow_html=True)
+st.write("How do districts differ in terms of frequency of crime? Do all districts have similar arrest rates?")
+st.write("Click on a district or a line representing a district to highlight it. The average across all districts is marked in red.")
 
 @st.cache  # add caching so we load the data only once
 def load_district_arrests():
@@ -231,7 +247,7 @@ def load_district_arrests():
     return data
 
 data = load_district_arrests()
-st.write(data)
+#st.write(data)
 
 #### new stuff from vivian ####
 
@@ -256,8 +272,8 @@ district_map = alt.Chart(data_geojson_remote).mark_geoshape(
 
 # Points and text
 hover = alt.selection(type='single', on='mouseover', nearest=True,
-                      fields=['latitude', 'longitude', 'district'])
-selector = selector = alt.selection_single(empty='all', fields=['district'])
+                      fields=['latitude', 'longitude','district'])
+selector = alt.selection_single(empty='all', fields=['district'])
 
 base = alt.Chart(data).encode(
     longitude='longitude:Q',
@@ -276,44 +292,50 @@ points = base.mark_point().encode(
 ### graph of counts by district over time
 start_year, end_year = st.slider("Start Year", 2001, 2020, (2001, 2020))
 
-base = chart = alt.Chart(data).mark_line(interpolate='basis').transform_filter(
+base = alt.Chart(data).mark_line(interpolate='basis').transform_filter(
     (start_year <= alt.datum['year']) & (end_year >= alt.datum['year'])
+)
+
+new_points = alt.Chart(data).mark_circle().encode(
+    opacity=alt.value(0)
+).add_selection(
+    hover
+)
+
+new_lines = alt.Chart(data).mark_line(interpolate='basis').encode(
+    size=alt.condition(~hover, alt.value(1), alt.value(3)),
+    opacity=alt.condition(hover, alt.value(1), alt.value(0.3))
 )
 
 avg_count = base.encode(
     x='year:O',
     y='mean(count_district)',
-    color=alt.value('green')
+    color=alt.value('#F63366')
 )
 
 new_chart = base.encode(
     x='year:O',
     y='count_district',
-    color=alt.condition(~selector, 'district:O', alt.value('red')),
-    opacity=alt.condition(selector,alt.value(1),alt.value(0.3))
-)
+    color='district',
+    opacity=alt.condition(selector,alt.value(1),alt.value(0.2)),
+    tooltip='district'
+).add_selection(selector)
 
-chart = alt.Chart(data).mark_line(interpolate='basis').transform_filter(
-    (start_year <= alt.datum['year']) & (end_year >= alt.datum['year'])
-).encode(
-    x='year:O',
-    y='count_district',
-    color=alt.condition(~selector, 'district:O', alt.value('red')),
-    opacity=alt.condition(selector,alt.value(1),alt.value(0.3))
-)
+
 
 avg_arrests = base.encode(
     x='year:O',
     y='mean(arrest_rate)',
-    color=alt.value('green')
+    color=alt.value('#F63366')
 )
 
 new_arrests = base.encode(
     x='year:O',
     y='arrest_rate:Q',
-    color=alt.condition(~selector, 'district:O', alt.value('red')),
-    opacity=alt.condition(selector,alt.value(1),alt.value(0.3))
-)
+    color='district',
+    opacity=alt.condition(selector,alt.value(1),alt.value(0.2)),
+    tooltip='district'
+).add_selection(selector)
 
 arrests = alt.Chart(data).mark_line(interpolate='basis').transform_filter(
     (start_year <= alt.datum['year']) & (end_year >= alt.datum['year'])
@@ -326,3 +348,42 @@ arrests = alt.Chart(data).mark_line(interpolate='basis').transform_filter(
 
 st.write(district_map + points + text | new_chart + avg_count | new_arrests + avg_arrests)
 
+
+###############################
+###        ML models        ###
+###############################
+
+#st.sidebar.write(comp)
+
+# select crime type
+type_values = ['arson', 'assault', 'battery', 'burglary', 'concealed carry license violation', 'criminal damage', 'criminal sexual assault', 'criminal trespass', 'crim sexual assault', 'deceptive practice', 'domestic violence', 'gambling', 
+'homicide', 'human trafficking', 'interference with public officer', 'intimidation', 'kidnapping', 'liquor law violation', 'motor vehicle theft', 'narcotics', 'non - criminal', 'non-criminal', 'non-criminal (subject specified)', 'obscenity', 'offense involving children', 'other narcotic violation', 'other offense', 'prostitution', 'public indecency', 'public peace violation', 'ritualism', 'robbery', 'sex offense', 'stalking', 'theft', 'weapons violation']
+options = list(range(len(type_values)))
+select_type = st.sidebar.selectbox("crime type", options, format_func=lambda x: type_values[x])
+
+# st.write(type_values[select_type])
+
+# select community area
+beat_values = [111, 112, 113, 114, 121, 122, 123, 124, 131, 132, 133, 134, 211, 212, 213, 214, 215, 221, 222, 223, 224, 225, 231, 232, 233, 234, 235, 310, 311, 312, 313, 314, 321, 322, 323, 324, 331, 332, 333, 334, 411, 412, 413, 414, 421, 
+422, 423, 424, 430, 431, 432, 433, 434, 511, 512, 513, 522, 523, 524, 531, 532, 533, 611, 612, 613, 614, 621, 622, 623, 624, 631, 632, 633, 634, 711, 712, 713, 714, 715, 722, 723, 724, 725, 726, 731, 732, 733, 734, 735, 811, 812, 813, 814, 815, 821, 822, 823, 824, 825, 831, 832, 833, 834, 835, 911, 912, 913, 914, 915, 921, 922, 923, 924, 925, 931, 932, 933, 934, 935, 1011, 1012, 1013, 1014, 1021, 1022, 1023, 1024, 1031, 1032, 1033, 1034, 1111, 1112, 1113, 1114, 1115, 1121, 1122, 1123, 1124, 1125, 1131, 1132, 1133, 1134, 1135, 1211, 1212, 1213, 1214, 1215, 1221, 1222, 1223, 1224, 1225, 1231, 1232, 1233, 1234, 1235, 1311, 1312, 1313, 1322, 1323, 1324, 1331, 1332, 1333, 1411, 1412, 1413, 1414, 1421, 1422, 1423, 1424, 1431, 1432, 1433, 1434, 1511, 1512, 1513, 1522, 1523, 1524, 1531, 1532, 1533, 1611, 1612, 1613, 1614, 1621, 1622, 1623, 1624, 1631, 1632, 1633, 1634, 1651, 1652, 1653, 1654, 1655, 
+1711, 1712, 1713, 1722, 1723, 1724, 1731, 1732, 1733, 1811, 1812, 1813, 1814, 1821, 1822, 1823, 1824, 1831, 1832, 1833, 1834, 1911, 1912, 1913, 1914, 1915, 1921, 1922, 1923, 1924, 1925, 1931, 1932, 1933, 1934, 1935, 2011, 2012, 2013, 2022, 2023, 2024, 2031, 2032, 2033, 2111, 2112, 2113, 2122, 2123, 2124, 2131, 2132, 2133, 2211, 2212, 2213, 2221, 2222, 2223, 2232, 2233, 2234, 2311, 2312, 2313, 2322, 2323, 2324, 2331, 2332, 2333, 2411, 2412, 2413, 2422, 2423, 2424, 2431, 2432, 2433, 2511, 2512, 2513, 2514, 2515, 2521, 2522, 2523, 2524, 2525, 2531, 2532, 2533, 2534, 2535]
+options = list(range(len(beat_values)))
+select_beat = st.sidebar.selectbox("beat", options, format_func=lambda x: beat_values[x])
+
+domestic = st.sidebar.checkbox("Domestic")
+
+crime_input = pd.DataFrame(columns = cols)
+crime_input.loc[0, 'primary_type'] = type_values[select_type]
+crime_input.loc[0, 'domestic'] = domestic
+crime_input.loc[0, 'beat'] = beat_values[select_beat]
+crime_input.loc[0, 'year'] = 2020
+st.sidebar.write(crime_input)
+
+
+st.sidebar.text("")
+st.sidebar.text("")
+predicted = clf.fit(X_train, y_train).predict(crime_input.loc[[0]])
+if predicted:
+    st.sidebar.text("prediction: Arrested!")
+else:
+    st.sidebar.text("prediction: Not Arrested!")
